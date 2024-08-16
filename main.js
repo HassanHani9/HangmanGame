d0044cde6b4ae01445737e3d8d5be59ee7dbd56f
hassanhani <vsonn2022@gmail.com>
2024-08-16 17:03:32 +0300

    adding json file and fetch data

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
+//decleration variables
 let letters = 'abcdefghijklmnopqrstuvwxyz'
 let lettersArr = Array.from(letters)
 let letterplace = document.querySelector('.left')
+
+//coping letters in the html page
 lettersArr.forEach(letter =>{
     let span = document.createElement('span')
     span.textContent = letter;
     letterplace.appendChild(span)
 })
+
+//loop on letters
+document.querySelectorAll('.left > span').forEach(span=>{
+//onclick on the span
+    span.addEventListener('click',()=>{
+
+    })
+})
+
+//fetch data from json file
+
+const data = fetch("/json.json").then(res=>{
+    let data = res.json()
+    return data
+}).then(data =>{
+    console.log(Object.keys(data))
+})
+
+console.log(data)
